dc81f5bbaf165687c342e1abb3acdfb3890e230d
ferinabay <ferinabayu@gmail.com>
2020-11-06 07:30:32 +0700

    add scene guide

---
+++ Assets/Script/menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+     public void PindahHalaman(string halaman)
+    {
+        SceneManager.LoadScene(halaman);
+    }
+    public void GoToMainMenu()
+    {
+        Application.LoadLevel("main_menu");
+    }
+    public void GoToARCamera()
+    {
+        Application.LoadLevel("ARCameraMain");
+    }
+    public void GoToGuideScene()
+    {
+        Application.LoadLevel("scene_guide");
+    }
+    public void exitaplikasi()
+    {
+        Application.Quit();
+    }
+}
